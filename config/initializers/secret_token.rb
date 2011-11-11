09fb15c4924e07127313e3a3f571a8b771365c07
Sachin Ranchod <sachin.ranchod@gmail.com>
2011-11-11 15:07:24 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpa::Application.config.secret_token = 'b0b9bc556e4ef93065a6480a3e3972869596b879d80a9859c47d8c5d0e7d7a43dfec63d54dc12dc21d0a0c98179cda664360f6c2d44dadc79a1db04762e9d3f4'
